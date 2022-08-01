89659fc2415eda0570cea91e7fa417644674a766
oopsiecodie <107747695+oopsiecodie@users.noreply.github.com>
2022-08-01 22:05:07 +0530

    Update mint-nft.js

--- components/web3/mint-nft.js
+++ components/web3/mint-nft.js
@@ -1,7 +1,7 @@
 import { Grid, Stack } from '@mui/material';
 import { useWeb3React } from '@web3-react/core';
 import { useEffect, useState } from 'react';
-import {  mintPublic, mintWhitelist, sampleNFT } from '@pages/utils/_web3';
+import {  mintPublic, mintCool, sampleNFT } from '@pages/utils/_web3';
 import MintNFTCard from './mint-nft-card';
 import useSWR from 'swr';
 import Web3 from 'web3';
@@ -16,46 +16,17 @@ const MintNFT = () => {
   const fetcher = (url) => fetch(url).then((res) => res.json());
   const { active, account, chainId } = useWeb3React();
 
-  const [whitelistClaimable, setWhitelistClaimable] = useState(NOT_CLAIMABLE);
-  const [alreadyClaimed, setAlreadyClaimed] = useState(false);
-
-
-  const [whitelistMintStatus, setWhitelistMintStatus] = useState();
+  const [CoolMintStatus, setCoolMintStatus] = useState();
   const [publicMintStatus, setPublicMintStatus] = useState();
 
-  const [numToMint, setNumToMint] = useState(2);
+  const [numToMint, setNumToMint] = useState(1);
 
-
-
- 
-
-  useEffect(() => {
-    if (!active || !whitelistValid) {
-      setWhitelistClaimable(NOT_CLAIMABLE);
-      return;
-    } else if (alreadyClaimed) {
-      setWhitelistClaimable(ALREADY_CLAIMED);
-      return;
-    }
-    async function validateClaim() {
-      const amount = (numToMint * 0.00).toString();
-      const amountToWei = web3.utils.toWei(amount, 'ether');
-      sampleNFT.methods.mintWhitelist(numToMint).call({ from: account, value: amountToWei }).then(() => {
-        setWhitelistClaimable(CLAIMABLE);
-      }).catch((err) => {
-        if (err.toString().includes('claimed')) { setWhitelistClaimable(ALREADY_CLAIMED)}
-        else { setWhitelistClaimable(NOT_CLAIMABLE) }
-      });
-    }
-    validateClaim();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[whitelistProof])
-
-  const onMintWhitelist = async () => {
-    const { success, status } = await mintWhitelist(account, numToMint);
+  const onCoolMint = async () => {
+    const { success, status } = await mintCool(account, numToMint);
     console.log(status);
-    setWhitelistMintStatus(success);
+    setCoolMintStatus(success);
   };
+
 
   const onPublicMint = async () => {
     const { success, status } = await mintPublic(account, numToMint);
@@ -71,19 +42,19 @@ const MintNFT = () => {
 
   <Grid item> 
             <MintNFTCard
-              title={'Whitelist Mint'}
+              title={'Bouncy Mint'}
               description={'Cost: 1 Free/wallet'}
-              canMint={whitelistClaimable}
-              mintStatus={whitelistMintStatus}
+              canMint={active}
+              mintStatus={CoolMintStatus}
               showNumToMint={true}
               setNumToMint={setNumToMint}
-              action={onMintWhitelist}
+              action={onCoolMint}
             />
           </Grid>
           <Grid item>
             <MintNFTCard
-              title={'Public Mint'}
-              description={'0.01 ETH'}
+              title={'Bonus Mint'}
+              description={'0.01 ETH, 10 Max/wallet'}
               canMint={active}
               mintStatus={publicMintStatus}
               showNumToMint={true}
